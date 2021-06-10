5c3879c0fc7c2142d294d3d807669ccd619d32af
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-06-11 01:29:08 +0800

    merged FFT and IFFT but has bug@@

--- fft_test.py
+++ fft_test.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.fftpack import fft,ifft
 import time
 
+import math
 
 class Polynomial:
 
@@ -21,60 +22,73 @@ class Polynomial:
 a = Polynomial([1, 2,1], [2,1,0])
 b = Polynomial([1,-2,1], [2,1,0])
 
-# Number of samplepoints
+_PI = np.math.pi
+# Number of sample points
 N = 1024
 # sample spacing
 T = 1 / N
 
-def DFT_slow(x):
-    """Compute the discrete Fourier Transform of the 1D array x"""
+def DFT_slow(x, inverse : bool):
+    """discrete Fourier Transform of the 1D array x"""
+    tmp_PI = _PI if inverse else -_PI
     x = np.asarray(x, dtype=float)
     N = x.shape[0]
     n = np.arange(N)
     k = n.reshape((N, 1))
-    M = np.exp(-2j * np.pi * k * n / N)
+    M = np.exp(2j * tmp_PI * k * n / N)
     return np.dot(M, x)
 
-def IDFT_slow(X):
-    """Compute the discrete Fourier Transform of the 1D array x"""
-    X = np.asarray(X, dtype=float)
-    N = X.shape[0]
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.exp(2j * np.pi * k * n / N)
-    return np.dot(M, X) / N
-
-def FFT(x):
+def FFT_Recursive(x, inverse : bool = False):
+    """ FFT with recursive Cooley-Tukey Algorithm"""
     N = x.shape[0]
+    
     if (N & (N - 1)):
         raise ValueError("size of x must be a power of 2")
     # this cutoff should be optimized
+    # as recursion on small N does not help much
+    # rather do in in one N^2 call.
     elif N <= 16:
-        return DFT_slow(x)
+        if inverse:
+            return DFT_slow(x, True)/N
+        else:
+            return DFT_slow(x, False)
 
-    w = np.exp(-2j * np.pi * np.arange(N) / N)
-    x_even = FFT(x[::2])
-    x_odd = FFT(x[1::2])
+    tmp_PI = _PI if inverse else -_PI
+    w = np.exp(-2j * tmp_PI / N * np.arange(N))
+    x_even = FFT_Recursive(x[::2], inverse)
+    x_odd = FFT_Recursive(x[1::2], inverse)
     
     X = np.concatenate([x_even + w[: N>>1]*x_odd, x_even + w[ N>>1:]*x_odd])
     return X
 
-def IFFT(X):
-    N = X.shape[0]
-    if N == 1:
-        return X
+def FFT_iter(x, inverse : bool= False):
+    """Iter. version of the Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    tmp_PI = _PI if inverse else -_PI
+    N = x.shape[0]
+
     if (N & (N - 1)):
         raise ValueError("size of x must be a power of 2")
-    # this cutoff should be optimized
-    #elif N <= 16:
-    #    return IDFT_slow(X)
 
-    X_even = IFFT(X[::2])
-    X_odd = IFFT(X[1::2])
-    w = np.exp(2j * np.pi * np.arange(N) / N)
+    # N_min here is equivalent to the stopping condition above,
+    # and should be a power of 2
+    N_min = min(N, 16)
     
-    x = np.concatenate([X_even + w[: N>>1]*X_odd, X_even + w[ N>>1:]*X_odd])
-    return x / N
+    # Perform an O[N^2] DFT on all length-N_min sub-problems at once
+    n = np.arange(N_min)
+    k = n[:, None]
+    M = np.exp(-2j * _PI * n * k / N_min)
+    X = np.dot(M, x.reshape((N_min, -1)))
+    if inverse:
+        X/=N
+    # build-up each level of the recursive calculation all at once
+    while X.shape[0] < N:
+        X_even = X[:, :X.shape[1]>>1]
+        X_odd = X[:, X.shape[1]>>1:]
+        factor = np.exp(-1j * _PI * np.arange(X.shape[0]) / X.shape[0])[:, None]
+        X = np.vstack([X_even + factor * X_odd,
+                       X_even - factor * X_odd])
+    return X.ravel()
 
 def timeit(tgt_func, msg = '', rpt = 1):
     t = 0
@@ -84,20 +98,22 @@ def timeit(tgt_func, msg = '', rpt = 1):
     stop = time.time()
     print(f'{msg}\n>avg {(stop - start)*1000/rpt : 8.3f} ms per loop')
 
-
-
 x = np.linspace(0.0, N*T, N)
-y = 0.1*np.sin(50 * 2.0*np.pi*x) + 0.7*np.sin(110 * 2.0*np.pi*x) + 0.3*np.sin(470 * 2.0*np.pi*x)
+y = 0.75*np.sin(5 * 2.0*_PI*x) + 0.5*np.sin(250 * 2.0*_PI*x)# + 0.25*np.sin(400 * 2.0*_PI*x)
+
+xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+yf = FFT_iter(y, False)
+rec = FFT_iter(yf, True)
 
 #ToCompare
 newyf=fft(y)
 newrec=ifft(newyf)
 
-yf = FFT(y)
-xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
-rec = IFFT(yf)
+print(np.allclose(yf, newyf))
+print(np.allclose(rec, newrec))
 
 fig, ax = plt.subplots(5)
+
 ax[0].plot(x, y)
 ax[1].plot(xf, np.abs(newyf[:N//2]))
 ax[2].plot(xf, np.abs(yf[:N//2]))
@@ -105,6 +121,5 @@ ax[3].plot(x, rec.real)
 ax[4].plot(x, newrec.real)
 plt.show()
 
-timeit(lambda: FFT(y), 'FFT', 10)
-timeit(lambda: IFFT(y), 'IFFT', 10)
-#np.fft.fft(x)
+timeit(lambda: FFT_Recursive(y), 'FFT_recu', 10)
+timeit(lambda: FFT_iter(y), 'FFT_iter', 10)
