55ca899415c396be0293e4873834d4e7c34c2bc7
YukiMingX <yukimuraz0119@gmail.com>
2021-06-12 19:28:04 +0800

    add our 2DFFT function

--- fft_test.py
+++ fft_test.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
-from scipy.fftpack import fft,ifft,fft2,ifft2,fftshift
+from scipy.fftpack import fft, ifft, fft2, ifft2, fftshift
 import time
 import cv2
 import math
+
 
 class Polynomial:
     def __init__(self, coeficents, degrees=None):
@@ -19,8 +20,9 @@ class Polynomial:
         print(self.degree)
         return sum([self.coeficents[i]*x**self.degree[i] for i in range(len(self.coeficents))])
 
-a = Polynomial([1, 2,1], [2,1,0])
-b = Polynomial([1,-2,1], [2,1,0])
+
+a = Polynomial([1, 2, 1], [2, 1, 0])
+b = Polynomial([1, -2, 1], [2, 1, 0])
 
 _PI = np.math.pi
 # Number of sample points
@@ -28,7 +30,8 @@ N = 1024**2
 # sample spacing
 T = 1 / N
 
-def DFT_slow(x, inverse : bool = False):
+
+def DFT_slow(x, inverse: bool = False):
     """discrete Fourier Transform of the 1D array x"""
     tmp_PI = -_PI if inverse else _PI
     x = np.asarray(x, dtype=complex)
@@ -38,10 +41,11 @@ def DFT_slow(x, inverse : bool = False):
     M = np.exp(-2j * tmp_PI * k * n / N)
     return np.dot(M, x)/N if inverse else np.dot(M, x)
 
-def FFT_recu(x, inverse : bool = False):
+
+def FFT_recu(x, inverse: bool = False):
     """ FFT with recursive Cooley-Tukey Algorithm"""
     N = x.shape[0]
-    
+
     if (N & (N - 1)):
         raise ValueError("size of x must be a power of 2")
     # this cutoff should be optimized
@@ -54,11 +58,12 @@ def FFT_recu(x, inverse : bool = False):
     w = np.exp(-2j * tmp_PI / N * np.arange(N))
     x_even = FFT_recu(x[::2], inverse)
     x_odd = FFT_recu(x[1::2], inverse)
-    
-    X = np.concatenate([x_even + w[: N>>1]*x_odd, x_even + w[ N>>1:]*x_odd])
+
+    X = np.concatenate([x_even + w[: N >> 1]*x_odd, x_even + w[N >> 1:]*x_odd])
     return X/N if inverse else X
 
-def FFT_iter(x, inverse : bool= False):
+
+def FFT_iter(x, inverse: bool = False):
     """Iter. version of the Cooley-Tukey FFT"""
     x = np.asarray(x, dtype=complex)
     tmp_PI = -_PI if inverse else _PI
@@ -75,16 +80,35 @@ def FFT_iter(x, inverse : bool= False):
     k = n[:, None]
     M = np.exp(-2j * tmp_PI * n * k / N_min)
     X = np.dot(M, x.reshape((N_min, -1)))
-    
+
     # build-up each level of the recursive calculation all at once
     while X.shape[0] < N:
-        X_even = X[:, :X.shape[1]>>1]
-        X_odd = X[:, X.shape[1]>>1:]
-        factor = np.exp(-1j * tmp_PI * np.arange(X.shape[0]) / X.shape[0])[:, None]
+        X_even = X[:, :X.shape[1] >> 1]
+        X_odd = X[:, X.shape[1] >> 1:]
+        factor = np.exp(-1j * tmp_PI *
+                        np.arange(X.shape[0]) / X.shape[0])[:, None]
         X = np.vstack([X_even + factor * X_odd, X_even - factor * X_odd])
     return X.ravel()
 
-def timeit(tgt_func, msg = '', rpt = 1):
+
+def ImgFFTYuki(img):
+    x = np.zeros((img.shape[0], img.shape[1]))
+    for i in range(img.shape[0]):
+        x[i] = (FFT_iter(img[i, 0:img.shape[1]]))
+    for i in range(x.shape[1]):
+        x[0:x.shape[0], i] = (FFT_iter(x[0:x.shape[0], i]))
+    return x
+
+
+def ImgFFTJason(data, inverse: bool = False):
+    tmp = np.array(list(map(lambda row: FFT_iter(row), data)), dtype=complex)
+    tmp = tmp.T
+    tmp = np.array(list(map(lambda col: FFT_iter(col), tmp)), dtype=complex)
+    tmp = tmp.T
+    return tmp
+
+
+def timeit(tgt_func, msg='', rpt=1):
     t = 0
     start = time.time()
     for _ in range(rpt):
@@ -92,29 +116,36 @@ def timeit(tgt_func, msg = '', rpt = 1):
     stop = time.time()
     print(f'{msg}\n>avg {(stop - start)*1000/rpt : 8.3f} ms per loop')
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
-def img_transform(data, inverse : bool= False):
+def rgb2gray(rgb):
+    return np.dot(rgb[..., :3], [0.2989, 0.5870, 0.1140])
+
+
+def img_transform(data, inverse: bool = False):
     tmp = map(lambda x: FFT_iter(x), data)
     print(tmp)
     return data
 
 
-#x = np.linspace(0.0, N*T, N)
-#y = 0.75*np.sin(5 * 2.0*_PI*x) + 0.5*np.sin(250 * 2.0*_PI*x) + 0.25*np.sin(400 * 2.0*_PI*x)
-fig, ax = plt.subplots(3)
+# x = np.linspace(0.0, N*T, N)
+# y = 0.75*np.sin(5 * 2.0*_PI*x) + 0.5*np.sin(250 * 2.0*_PI*x) + 0.25*np.sin(400 * 2.0*_PI*x)
+fig, ax = plt.subplots(5)
 
-img = mpimg.imread('./ripple.png')
-#img = rgb2gray(img)
-
+# img = mpimg.imread('C:/Users/yukimura/Documents/Workplace/FFT_audio/squares.png')
+img = cv2.imread(
+    'C:/Users/yukimura/Documents/Workplace/FFT_audio/squares.png')
+img = rgb2gray(img)
 ax[0].imshow(img)
 
 imgf = fft2(img)
+imgjason = ImgFFTJason(img)
+imgyuki = ImgFFTYuki(img)
 ax[1].imshow(np.abs(fftshift(imgf)))
 
 rec = ifft2(imgf)
 ax[2].imshow(np.abs(rec))
+ax[3].imshow(np.abs(fftshift(imgyuki)))
+ax[4].imshow(np.abs(fftshift(imgjason)))
 
 plt.show()
 '''
@@ -122,7 +153,7 @@ xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
 yf = FFT_iter(y, False)
 rec = FFT_iter(yf, True)/N
 
-#To Compare
+# To Compare
 newyf=fft(y)
 newrec=ifft(newyf)
 
